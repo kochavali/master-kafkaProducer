d9ed00486bb2174e690c8d8765fe3bbc188360e6
kochavali <48750198+kochavali@users.noreply.github.com>
2026-09-18 21:49:27 +0530

    added test data

--- test.java
+++ test.java
@@ -1,16 +1,33 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static org.springframework.transaction.annotation.Isolation.READ_COMMITTED;
+import static org.springframework.transaction.annotation.Propagation.REQUIRED;
+
 public class test {
+  @Transactional(propagation=REQUIRED,isolation=READ_COMMITTED )
   public static void main(String[] args) {
-    List<Integer> list = new ArrayList<>();
-    IntStream.range(1, 1000).parallel().forEach(list::add);
-    System.out.println(list.size());
+    List<List<String>> masterlist = new ArrayList<>();
+    List<String> list1 = new ArrayList<>();
+    list1.add("str");
+    list1.add("st2");
+    List<String> list11 = new ArrayList<>();
+
+    List<String> nonNullItems = list1.stream()
+      .filter(Objects::nonNull)
+      .collect(Collectors.toList());
+    list11.add("wor1");
+    list11.add("wor2");
+    masterlist.add(list1);
+    masterlist.add(list11);
+    System.out.println(masterlist);
+    //IntStream.range(1, 1000).parallel().forEach(list::add);
+    System.out.println(masterlist.size());
     String st="swiss";
     st.chars().mapToObj(c -> (char) c)
       .collect(Collectors.groupingBy(
@@ -26,6 +43,11 @@ public class test {
         ch -> System.out.println("First non-repeating character: " + ch),
         () -> System.out.println("No non-repeating character found.")
       );
+    Integer a=128;
+    System.out.println(a.toString());
+    Integer b=128;
+    System.out.println(a!=b);
   }
+
 
 }
